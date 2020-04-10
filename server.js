e1910466137ae2d1103cd9f1ea56ffb043b97fe7
KleinerMeteor <kapschitzki.p@web.de>
2020-04-10 11:29:50 +0200

    setup listener on port 3000 and first get route

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Journal your Trading!');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// basic get function
+app.get("/", (req, res) => {
+    res.send("Hello Trading Journal");
+});
+
+// setup listener
+app.listen(port, () => {
+    console.log("listening on " + port);
+});
