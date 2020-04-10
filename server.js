95b411fe617e4d07c195a78583dfacba93a9849f
KleinerMeteor <kapschitzki.p@web.de>
2020-04-10 19:05:26 +0200

    Backend: Integrated index.html

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const port = process.env.PORT || 3000;
 
 // basic get function
 app.get("/", (req, res) => {
-    res.send("Hello Trading Journal");
+    // res.send("Hello Trading Journal");
+    // console.log("dirname: " + __dirname);
+    res.sendFile(__dirname + "/views/index.html"); //Note: __dirname is the directory you are in.
 });
 
 // setup listener
