914450a569dcdc62862c6ccee4ee27f6313a7b06
KleinerMeteor <kapschitzki.p@web.de>
2020-04-11 10:54:08 +0200

    Backend: Installed and setup body-parser and installed mongoose and connected to database 'trading-journal' - also changed quotation marks from double to single quotation markes where used

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // Middleware
+// const MongoClient = require('mongodb').MongoClient; // For using mongodb with the native driver instead of mongoose
+const mongoose = require('mongoose');
+const url = 'mongodb://127.0.0.1:27017/trading-journal';
 const app = express();
 const port = process.env.PORT || 3000;
 
-// basic get function
+// Make sure you place body-parser before the CRUD handlers!
+// body-parser is a middleware to tidy up the request object before use
+// urlencoded extracts data from <form> element and adds them to
+// 'body' property in the request object
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Connecting to database
+mongoose.connect(url, { useNewUrlParser: true });
+
+// Check if connection works
+const db = mongoose.connection;
+db.once('open', _ => {
+    console.log('Database connected: ', url);
+});
+
+db.on('error', err => {
+    console.log('connection error: ', err);
+});
+
+// Basic get function
 app.get("/", (req, res) => {
     // res.send("Hello Trading Journal");
     // console.log("dirname: " + __dirname);
-    res.sendFile(__dirname + "/views/index.html"); //Note: __dirname is the directory you are in.
+    res.sendFile(__dirname + '/views/index.html'); //Note: __dirname is the directory you are in.
 });
 
-// post route for writing into database
-app.post("/trades", (req, res) => {
-    console.log("Posting my trades!");
+// POST route for writing into database
+app.post('/trades', (req, res) => {
+    // console.log("Posting my trades!");
+    console.log(req.body);
 });
 
-// setup listener
+// Setup listener
 app.listen(port, () => {
-    console.log("listening on " + port);
+    console.log('listening on ' + port);
 });
