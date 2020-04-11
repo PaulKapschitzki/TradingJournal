b3be2fe4805c3da552d0012a1f628e4c8bf9d447
KleinerMeteor <kapschitzki.p@web.de>
2020-04-11 11:21:11 +0200

    Backend: Added model file for trades; Added basic Schema for a trade entry into database;

---
+++ models/trades.js
@@ -0,0 +1,24 @@
+/**
+ * Creating a Schema for the 'trades' collection in 'trading-journal' database 
+ */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tradesSchema = new Schema({
+    // determining the data type for used values
+    symbole: String,
+    direction: String,
+    entryPrice: Number,
+    exitPrice: Number,
+    stopLoss: Number,
+    firstTraget: Number,
+    secondTarget: Number,
+    thirdTarged: Number,
+    entryDate: Date,
+    exitDate: Date,
+    exitDateFirstTarget: Date,
+    exitDateSecondTarget: Date,
+    exitDateThirdTarget: Date,
+    profitLossCurrency: Number,
+    profitLossPips: Number
+});
