5fb65cc22eea130141770bbc5020f8d5ef871d44
KleinerMeteor <kapschitzki.p@web.de>
2020-05-02 10:20:30 +0200

    Backend: Added features to database mongoose Schema for trade entries

--- models/trades.model.js
+++ models/trades.model.js
@@ -6,27 +6,35 @@ const Schema = mongoose.Schema;
 
 const TradesSchema = new Schema({
     // determining the data type for used values
-    numberPositions: {type: Number, required: true, default: 2},
-    status: {type: String, required: true, default: 'active'}, // active or closed
-    symbole: {type: String, required: true, max: 6}, // Forex symbole
-    direction: {type: String, required: true, max: 4}, // buy or sell (long or short)
-    entryPrice: {type: Number, required: true},
-    exitPrice: {type: Number, required: true},
-    stopLoss: {type: Number, required: true},
-    firstTraget: {type: Number, required: true},
-    secondTarget: {type: Number, required: true},
-    thirdTarged: {type: Number, required: true},
-    created: {type: Date, required: true, default: Date.now},
-    entryDate: Date,
-    exitDate: Date,
-    exitDateFirstTarget: Date,
-    exitDateSecondTarget: Date,
-    exitDateThirdTarget: Date,
-    profitLossCurrency: Number,
-    profitLossPips: Number,
-    notesEntry: String,
-    notesExit: String,
-    notesGeneral: String
+    tradeNumber:            {type: Number, required: true},
+    numberPositions:        {type: Number, required: true, default: 2},
+    status:                 {type: String, required: true, default: 'Open'}, // Open, Active or Closed
+    symbole:                {type: String, required: true, max: 6}, // Forex symbole
+    setup:                  {type: String, required: true}, // Pinbar, 2BarReversal, ...
+    timeframe:              {type: String, required: true, default: 'D1'}, // M1, M5, M15, M30, H1, H4, D1, W1, M1
+    rating:                 {type: Number}, // rating of the setup, calculated through serveral factors
+    direction:              {type: String, required: true, max: 4}, // buy or sell (long or short)
+    volume:                 {type: Number, required: true, default: 0.01}, // Lots
+    entryPrice:             {type: Number, required: true},
+    exitPrice:              {type: Number, required: true},
+    stopLoss:               {type: Number, required: true},
+    firstTraget:            {type: Number, required: true},
+    secondTarget:           {type: Number, required: true},
+    thirdTarged:            {type: Number, required: true},
+    created:                {type: Date, required: true, default: Date.now},
+    entryDate:              {type: Date, required: false, default: Date.now},
+    exitDate:               {type: Date, required: false, default: Date.now},
+    exitDateFirstTarget:    {type: Date, required: false, default: Date.now},
+    exitDateSecondTarget:   {type: Date, required: false, default: Date.now},
+    exitDateThirdTarget:    {type: Date, required: false, default: Date.now},
+    result:                 {type: String}, // Profit, Loss, BreakEven, Cancelled
+    profitLossCurrency:     {type: Number, required: false},
+    profitLossPips:         {type: Number, required: false},
+    notesEntry:             {type: String, required: true},
+    notesExit:              {type: String},
+    notesGeneral:           {type: String},
+    accordingToPlan:        {type: String, required: false}, // Yes or No
+    image:                  {type: BSON}
 });
 
 // Set the collection 'trades' for this Schema
