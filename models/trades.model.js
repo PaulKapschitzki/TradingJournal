faf741557172460ec95128180dce70717d2bf342
KleinerMeteor <kapschitzki.p@web.de>
2020-05-09 13:49:23 +0200

    Backend: Added fields for second and third positions to model and controller.

--- models/trades.model.js
+++ models/trades.model.js
@@ -6,40 +6,33 @@ const Schema = mongoose.Schema;
 
 const TradesSchema = new Schema({
     // determining the data type for used values
-    tradeNumber:            {type: Number, required: true},
-    numberPositions:        {type: Number, min: [1, 'Must have at lease one position'], required: [true, 'Why no positions?'], default: 2},
-    type:                   {type: String, required: true, enum: ['Pending Order', 'Market Order'], default: 'Pending Order'}, // "Pending Order" or "Market Order"
-    pendingOrderType:       {type: String, required: false, enum: ['Buy Limit', 'Buy Stop', 'Sell Limit', 'Sell Stop']},
-    status:                 {type: String, required: true, enum: ['Open', 'Active', 'Closed'], default: 'Open'}, // Open, Active or Closed
-    symbole:                {type: String, required: true, max: 6}, // Forex symbole
-    setup:                  {type: String, required: true}, // Pinbar, 2BarReversal, ...
-    timeframe:              {type: String, required: true, enum: ['M1', 'M5', 'M15', 'M30', 'H1', 'H4', 'D1', 'W1', 'M1'], default: 'D1'}, // M1, M5, M15, M30, H1, H4, D1, W1, M1
-    rating:                 {type: Number}, // rating of the setup, calculated through serveral factors
-    direction:              {type: String, required: true, max: 4, enum: ['Buy', 'Sell']}, // buy or sell (long or short)
-    volume:                 {type: Number, required: true, default: 0.10}, // Lots
-    entryPrice:             {type: Number, required: true},
-    exitPrice:              {type: Number, required: true},
-    stopLoss:               {type: Number, required: true},
-    takeProfit:             {type: Number, required: false},
-    riskReturnRatio:        {type: Number},
-    firstTraget:            {type: Number, required: true}, // DEPRECATED?
-    secondTarget:           {type: Number, required: true}, // DEPRECATED?
-    thirdTarget:            {type: Number, required: true}, // DEPRECATED?
-    created:                {type: Date, required: true, default: Date.now},
-    entryDate:              {type: Date, required: false, default: Date.now},
-    exitDate:               {type: Date, required: false, default: Date.now},
-    exitDateFirstTarget:    {type: Date, required: false, default: Date.now}, // DEPRECATED?
-    exitDateSecondTarget:   {type: Date, required: false, default: Date.now}, // DEPRECATED?
-    exitDateThirdTarget:    {type: Date, required: false, default: Date.now}, // DEPRECATED?
-    result:                 {type: String, enum: ['Profit', 'Loss', 'Breakeven', 'Cancelled'],}, // Profit, Loss, BreakEven, Cancelled
-    profitLossCurrency:     {type: Number, required: false},
-    profitLossPips:         {type: Number, required: false},
-    commentsEntry:          {type: String, required: true},
-    commentsExit:           {type: String},
-    commentsGeneral:        {type: String},
-    accordingToPlan:        {type: String, required: false}, // Yes or No
-    imageEntry:             {type: Buffer},
-    imageExit:              {type: Buffer}
+    tradeNumber:                {type: Number, required: true},
+    numberPositions:            {type: Number, min: [1, 'Must have at lease one position'], required: [true, 'Why no positions?'], default: 2},
+    type:                       {type: String, required: true, enum: ['Pending Order', 'Market Order'], default: 'Pending Order'}, // "Pending Order" or "Market Order"
+    pendingOrderType:           {type: String, required: false, enum: ['Buy Limit', 'Buy Stop', 'Sell Limit', 'Sell Stop']},
+    status:                     {type: String, required: true, enum: ['Open', 'Active', 'Closed'], default: 'Open'}, // Open, Active or Closed
+    symbole:                    {type: String, required: true, max: 6}, // Forex symbole
+    setup:                      {type: String, required: true}, // Pinbar, 2BarReversal, ...
+    timeframe:                  {type: String, required: true, enum: ['M1', 'M5', 'M15', 'M30', 'H1', 'H4', 'D1', 'W1', 'M1'], default: 'D1'}, // M1, M5, M15, M30, H1, H4, D1, W1, M1
+    rating:                     {type: Number}, // rating of the setup, calculated through serveral factors
+    direction:                  {type: String, required: true, max: 4, enum: ['Buy', 'Sell']}, // buy or sell (long or short)
+    volume:                     {type: Number, required: true, default: 0.10}, // Lots
+    entryPrice:                 {type: Number, required: true},
+    stopLoss:                   {type: Number, required: true},
+    takeProfit:                 {type: Number, required: false},
+    riskReturnRatio:            {type: Number},
+    created:                    {type: Date, required: true, default: Date.now},
+    entryDate:                  {type: Date, required: false, default: Date.now},
+    exitDate:                   {type: Date, required: false, default: Date.now},
+    result:                     {type: String, required: false, enum: ['Profit', 'Loss', 'Breakeven', 'Cancelled'],}, // Profit, Loss, BreakEven, Cancelled
+    profitLossCurrency:         {type: Number, required: false},
+    profitLossPips:             {type: Number, required: false},
+    commentsEntry:              {type: String, required: true},
+    commentsExit:               {type: String},
+    commentsGeneral:            {type: String},
+    accordingToPlan:            {type: String, required: false}, // Yes or No
+    imageEntry:                 {type: Buffer},
+    imageExit:                  {type: Buffer}
 });
 
 // Set the collection 'trades' for this Schema
