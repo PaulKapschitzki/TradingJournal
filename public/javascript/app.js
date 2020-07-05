4798084d6f359b28d800ffe6626e591f98c6a6e0
PaulKapschitzki <kapschitzki.p@web.de>
2020-07-05 10:17:46 +0200

    Frontend: Changed Postion and Size of Window

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -25,7 +25,7 @@ $(document).ready(() => {
         window.open(
             '/api/trades/create',
             'New Trade Entry',
-            'width=900px,height=910px,menubar=yes,location=yes,resizable=no,scrollbars=yes,status=yes'
+            'width=800px,height=1000px,top=200px,left=900px,toolbar=no,menubar=no,location=yes,resizable=no,scrollbars=yes,status=yes'
         );
     });
 });
