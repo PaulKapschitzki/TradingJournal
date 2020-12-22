b93b2d2ac8a76da90700fc01f4c555a4ec31eb85
PaulKapschitzki <kapschitzki.p@web.de>
2020-12-22 19:43:00 +0100

    Get _id from clicked row via "Edit button"

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -28,4 +28,56 @@ $(document).ready(() => {
             'width=800px,height=1000px,top=200px,left=900px,toolbar=no,menubar=no,location=yes,resizable=no,scrollbars=yes,status=yes'
         );
     });
+
+    $('.tradeTable').on('click', '.editRow', (event) => {
+        console.log('Edit button was clicked');
+        // updateTrade(this.parent);
+        console.log(event.target.parentNode.parentNode.id);
+        updateTrade(event.target.parentNode.parentNode.id);
+    });
+
+    $('.tradeTable').on('click', '.deleteTrade', (event) => {
+        event.stopPropagation(); // stops the event from "bubbling up" or we can delete items without toggling them 'done' or not
+        // removeTrade($(this).parent());
+    });
 });
+
+const updateTrade = (tradeId) => {
+    window.open(
+        '/api/trades/' + tradeId,
+        'Update Trade Entry',
+        'width=800px,height=1000px,top=200px,left=900px,toolbar=no,menubar=no,location=yes,resizable=no,scrollbars=yes,status=yes'
+    );
+    //console.log(trade.data('closed'));
+    var updateURL = '/api/trades/' + tradeId;
+    $.ajax({
+        method: 'PUT',
+        url: updateURL,
+        data: updateData
+    })
+    .then((updatedTrade) => {
+        console.log(updatedTrade);
+        // trade.toggleClass("done");
+        // trade.data('closed', isOpen); // save current value for 'completed' in the invisable data property
+    })
+}
+
+const removeTrade = (thisParent) => {
+    //$(this).parent().remove();
+    //var clickedId = $(this).parent().data('id'); // save id-data-attribute from addTodoToList function
+    var clickedId = thisParent.data('id'); // save id-data-attribute from addTodoToList function
+    var deleteURL = '' + clickedId + '/delete';
+    console.log("clickedId = " + clickedId);
+    
+    $.ajax({
+        method: 'DELETE',
+        url: deleteURL
+    })
+    .then((data) => {
+        console.log(data);
+        thisParent.remove()// remove directly on click from trade list
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
